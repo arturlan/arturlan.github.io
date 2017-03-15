bd4dc4780e01fb6e5e25129ff4b533d5d8d399a7
Artur Lane <arturlan.us@gmail.com>
2017-03-14 23:31:27 -0400

    update

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,24 @@
-var body = $('body');
 
 //canvas background
 var background = function() {
-	var canvas1 = document.getElementById('canvasOne');
-	var ctx = canvas1.getContext('2d');
-
-	ctx.fillStyle = 'rgba(0,200,0,1)';
-	ctx.fillRect(36, 10, 22, 22);
+	var canvasBackground = document.getElementById('canvasOne');
+	var ctx = canvasBackground.getContext('2d');
 }
-//game engine learned from edX HTML5 Part 2: Advanced Techniques.
 
+//game engine learned from edX HTML5 Part 2: Advanced Techniques.
 var GE = function() {
 
 	var mainLoop = function(time) {
 		background();
 		requestAnimationFrame(mainLoop);
 	};
-/*
+
 	var start = function() {
 		requestAnimationFrame(mainLoop);
 	};
-*/
+
 	return {
-		start: mainLoop
+		start: start
 	};
 }
 
