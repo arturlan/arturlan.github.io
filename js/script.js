ce2c3360a2b95574603795dcee343f6f27ba063a
Artur Lan <arturlan.us@gmail.com>
2017-05-24 23:20:44 -0400

    15%

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-    $("#developing").click(function(){
+    $("#running").click(function(){
         console.log('clicking');
+    });
+
+    $("#name a").mouseover(function(){
+      $("#name a").css("text-decoration", "underline");
+      console.log("over");
+    });
+
+    $("#playing").mouseenter(function(){
+      $("#main").fadeTo(300, 0, "linear");
+      console.log("playing");
+    });
+
+    $("#playing").mouseleave(function(){
+      $("#main").fadeTo(300, 1, "linear");
     });
 });
